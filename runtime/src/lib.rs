33b4bdba1bde864148aeac0588671b83967e5037
Thanos <56822898+thadouk@users.noreply.github.com>
2026-09-21 16:55:43 +0100

    chore: enable node-manager pallet
    Removes the feature flag gating the node-manager pallet and properly
    includes the pallet to PRDCTR runtime.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -305,8 +305,6 @@ mod runtime {
     #[runtime::pallet_index(92)]
     pub type SignedNeoSwaps = pallet_pm_signed_neo_swaps;
 
-    // Feature flags are not supported within runtime macro till version 33.0.1
-    // To enable the pallet, uncomment the two lines below and build with `--features node-manager`.
-    // #[runtime::pallet_index(93)]
-    // pub type NodeManager = pallet_node_manager;
+    #[runtime::pallet_index(93)]
+    pub type NodeManager = pallet_node_manager;
 }
